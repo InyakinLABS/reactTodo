e388b3772eddc175c37a2bb2f9abe8a560b78dc6
InyakinLABS <dima09561@gmail.com>
2025-03-20 16:18:26 +0300

    linted code

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,62 +1,56 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class NewTaskForm extends Component {
-  constructor() {
-    super()
-    this.state = {
-      value: '',
-      minutes: '',
-      seconds: '',
-    }
-  }
+const NewTaskForm = (props) => {
+  const [value, setValue] = useState('')
+  const [minutes, setMinutes] = useState('')
+  const [seconds, setSeconds] = useState('')
 
-  static propTypes = {
-    addItem: PropTypes.func.isRequired,
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const { value, minutes, seconds } = this.state
-
     const timeInSeconds = parseInt(minutes || 0) * 60 + parseInt(seconds || 0)
 
     if (value.trim()) {
-      this.props.addItem(value, timeInSeconds)
-      this.setState({ value: '', minutes: '', seconds: '' })
+      props.addItem(value, timeInSeconds) // Передаем текст задачи и время
+      setValue('')
+      setMinutes('')
+      setSeconds('') // Очищаем поля формы
     }
   }
 
-  render() {
-    return (
-      <form className="header" onSubmit={this.handleSubmit}>
-        <h1>TODO</h1>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-          value={this.state.value}
-          onChange={(e) => this.setState({ value: e.target.value })}
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Min"
-          type="number"
-          min="0"
-          value={this.state.minutes}
-          onChange={(e) => this.setState({ minutes: e.target.value })}
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          type="number"
-          min="0"
-          max="59"
-          value={this.state.seconds}
-          onChange={(e) => this.setState({ seconds: e.target.value })}
-        />
-        <button type="submit" style={{ display: 'none' }} />
-      </form>
-    )
-  }
+  return (
+    <form className="header" onSubmit={handleSubmit}>
+      <h1>TODO</h1>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <input
+        className="new-todo-form__timer"
+        placeholder="Min"
+        type="number"
+        min="0"
+        value={minutes}
+        onChange={(e) => setMinutes(e.target.value)}
+      />
+      <input
+        className="new-todo-form__timer"
+        placeholder="Sec"
+        type="number"
+        min="0"
+        max="59"
+        value={seconds}
+        onChange={(e) => setSeconds(e.target.value)}
+      />
+      <button type="submit" style={{ display: 'none' }} /> {/* Скрытая кнопка для отправки формы */}
+    </form>
+  )
+}
+export default NewTaskForm
+
+NewTaskForm.propTypes = {
+  addItem: PropTypes.func.isRequired,
 }
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,94 +1,82 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-export default class Task extends Component {
-  constructor() {
-    super()
-    this.state = {
-      editing: false,
-      value: '',
-    }
-  }
-  static propTypes = {
-    todo: PropTypes.object.isRequired,
-    deleteItem: PropTypes.func.isRequired,
-    editItem: PropTypes.func.isRequired,
-    clickHandler: PropTypes.func.isRequired,
-    startTimer: PropTypes.func.isRequired,
-    stopTimer: PropTypes.func.isRequired,
-    timers: PropTypes.object.isRequired,
-  }
+const Task = ({ todo, deleteItem, editItem, clickHandler, startTimer, stopTimer, timers }) => {
+  const { value, id, date, checked, timeLeft } = todo
 
-  formatTime = (seconds) => {
+  const [editing, setEditing] = useState(false)
+  const [inputValue, setInputValue] = useState(value)
+
+  const isRunning = Boolean(timers[id])
+
+  const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60)
     const secs = seconds % 60
     return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
   }
-  handleSubmit(event) {
+
+  const formattedDate =
+    date && !isNaN(new Date(date).getTime())
+      ? formatDistanceToNow(new Date(date), { includeSeconds: true })
+      : 'just now'
+
+  const handleSubmit = (event) => {
     event.preventDefault()
-    const {
-      editItem,
-      todo: { id },
-    } = this.props
-    editItem(id, this.state.value)
-    this.setState({ value: '' })
-    this.setState({ editing: false })
+    editItem(id, inputValue)
+    setEditing(false)
   }
 
-  render() {
-    const { todo, deleteItem, clickHandler, startTimer, stopTimer, timers } = this.props
-    const { value, id, date, checked, timeLeft } = todo
-    let isRunning = Boolean(timers[id])
-
-    const formattedDate =
-      date && !isNaN(new Date(date).getTime())
-        ? formatDistanceToNow(new Date(date), { includeSeconds: true })
-        : 'just now'
-
-    return (
-      <li className={checked ? 'completed' : this.state.editing ? 'editing' : null}>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={checked}
-            onChange={(e) => {
-              clickHandler(id, e.target.checked)
-            }}
-          />
-          <label>
-            <span className="title">{value}</span>
-            <span className="description">
-              <button
-                className={`icon ${isRunning ? 'icon-pause' : 'icon-play'}`}
-                onClick={isRunning ? () => stopTimer(id) : () => startTimer(id)}
-              />
-              <span className="timer">{this.formatTime(timeLeft)}</span>
-              <span>
-                {' '}
-                <br></br>created {formattedDate} ago
-              </span>
-            </span>
-          </label>
-          <button
-            type="button"
-            onClick={() => this.setState(({ editing }) => ({ editing: !editing, value: this.props.todo.value }))}
-            className="icon icon-edit"
-          ></button>
-          <button className="icon icon-destroy" onClick={() => deleteItem(id)} />
-        </div>
-        {this.state.editing && (
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <input
-              onChange={(event) => this.setState({ value: event.target.value })}
-              type="text"
-              className="edit"
-              value={this.state.value}
+  return (
+    <li className={checked ? 'completed' : editing ? 'editing' : null}>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => clickHandler(id, e.target.checked)}
+        />
+        <label>
+          <span className="title">{value}</span>
+          <span className="description">
+            <button
+              className={`icon ${isRunning ? 'icon-pause' : 'icon-play'}`}
+              onClick={isRunning ? () => stopTimer(id) : () => startTimer(id)}
             />
-          </form>
-        )}
-      </li>
-    )
-  }
+            <span className="timer">{formatTime(timeLeft)}</span>
+            <span>
+              <br />
+              created {formattedDate} ago
+            </span>
+          </span>
+        </label>
+        <button
+          type="button"
+          onClick={() => {
+            setEditing(!editing)
+            setInputValue(value)
+          }}
+          className="icon icon-edit"
+        />
+        <button className="icon icon-destroy" onClick={() => deleteItem(id)} />
+      </div>
+      {editing && (
+        <form onSubmit={handleSubmit}>
+          <input onChange={(e) => setInputValue(e.target.value)} type="text" className="edit" value={inputValue} />
+        </form>
+      )}
+    </li>
+  )
 }
+
+Task.propTypes = {
+  todo: PropTypes.object.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  editItem: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+  startTimer: PropTypes.func.isRequired,
+  stopTimer: PropTypes.func.isRequired,
+  timers: PropTypes.object.isRequired,
+}
+
+export default Task
